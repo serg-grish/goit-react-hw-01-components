b51fa30c641d042f9e504d270f296a567ed3f220
Serg_Grish <balchyg@icloud.com>
2021-09-28 22:28:19 +0300

    16

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-import './App.css';
-import { container, section, sectionBottom } from './components/Container/container.module.css';
+import "./App.css";
+import {
+  container,
+  section,
+  sectionBottom,
+} from "./components/Container/container.module.css";
 
-import Profile from './components/Profile/Profile';
-import Statistics from './components/Statistics/Statistics';
-import FriendsList from './components/FriendsList/FriendsList';
-import Transactions from './components/Transactions/Transactions';
+import Profile from "./components/Profile/Profile";
+import Statistics from "./components/Statistics/Statistics";
+import FriendsList from "./components/FriendsList/FriendsList";
+import Transactions from "./components/Transactions/Transactions";
 
-import user from './data/user.json';
-import statisticalData from './data/statistical-data.json';
-import friends from './data/friends.json';
-import transactions from './data/transactions.json';
+import user from "./data/user.json";
+import statisticalData from "./data/statistical-data.json";
+import friends from "./data/friends.json";
+import transactions from "./data/transactions.json";
 
 function App() {
   return (
@@ -24,7 +28,6 @@ function App() {
         />
         <div className={container}>
           <Statistics title="Upload stats" stats={statisticalData} />
-          <Statistics stats={statisticalData} />
         </div>
       </div>
       <div className={sectionBottom}>
