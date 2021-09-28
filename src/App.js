fd8ab9b0c84977915e40b45fc82f5a1f09ac67e6
Serg_Grish <balchyg@icloud.com>
2021-09-28 22:15:16 +0300

    15

--- src/App.js
+++ src/App.js
@@ -1,19 +1,15 @@
-import "./App.css";
-import {
-  container,
-  section,
-  sectionBottom,
-} from "./components/Container/container.module.css";
+import './App.css';
+import { container, section, sectionBottom } from './components/Container/container.module.css';
 
-import Profile from "./components/Profile/Profile";
-import Statistics from "./components/Statistics/Statistics";
-import FriendsList from "./components/FriendsList/FriendsList";
-import Transactions from "./components/Transactions/Transactions";
+import Profile from './components/Profile/Profile';
+import Statistics from './components/Statistics/Statistics';
+import FriendsList from './components/FriendsList/FriendsList';
+import Transactions from './components/Transactions/Transactions';
 
-import user from "./data/user.json";
-import statisticalData from "./data/statistical-data.json";
-import friends from "./data/friends.json";
-import transactions from "./data/transactions.json";
+import user from './data/user.json';
+import statisticalData from './data/statistical-data.json';
+import friends from './data/friends.json';
+import transactions from './data/transactions.json';
 
 function App() {
   return (
